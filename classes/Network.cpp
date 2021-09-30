327e380d739d82393a354ed2d01c5f17ad5e141c
George Gantus <georgegantus@usp.br>
2021-09-30 18:31:17 -0300

    feat: implementing cross-entropy error method

--- classes/Network.cpp
+++ classes/Network.cpp
@@ -1,5 +1,7 @@
 #include "Network.h"
 
+#include <math.h>
+
 #include <iostream>
 Network::Network(int numInputs) {
     this->numInputs = numInputs;
@@ -27,6 +29,24 @@ vector<float> Network::_compute(vector<float> input) {
         input = (*layer).output(input);
     }
     return input;
+}
+
+float Network::_getBatchCrossEntropyError(vector<vector<float>> X, vector<vector<int>> Y) {
+    vector<vector<float>>::iterator input;
+    vector<vector<int>>::iterator target;
+    vector<float> output;
+    float meanError = 0;
+    for (input = X.begin(), target = Y.begin(); input < X.end(); input++, target++) {
+        output = _compute(*input);
+        float error = 0;
+        for (int i = 0; i < output.size(); i++) {
+            /* code */
+            error += log2(output[i]) * (*target)[i];
+        }
+        meanError += error;
+    }
+    meanError = meanError / Y.size();
+    return meanError;
 }
 void Network::print() {
     vector<Layer>::iterator layer;
--- classes/Network.h
+++ classes/Network.h
@@ -5,6 +5,7 @@ class Network {
    private:
     vector<Layer> layers;
     int numInputs;
+    float _getBatchCrossEntropyError(vector<vector<float>> X, vector<vector<int>> Y);
     vector<float> _compute(vector<float> input);
 
    public:
